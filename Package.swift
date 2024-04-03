4ad01fdfaa6c57f648ea2880be899a0fd22780cb
daisuke.ebina <daisuke.ebina@ardito.co.jp>
2024-04-03 16:06:58 +0900

    add

--- Package.swift
+++ Package.swift
@@ -15,19 +15,9 @@ let package = Package(
             targets: ["EbinaFrameworkWrapper"])    // <--- reference the wrapper here
     ],
     dependencies: [
-        .package(url: "https://github.com/Alamofire/Alamofire.git", from: "5.7.1"),
-        .package(url: "https://github.com/stasel/WebRTC.git", from: "115.0.0"),
-        .package(url: "https://github.com/daltoniam/Starscream.git", from: "4.0.6"),
-        .package(url: "https://github.com/AudioKit/AudioKit", from: "5.6.0"),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", from: "6.6.0"),
-        .package(url: "https://github.com/konkab/AlamofireNetworkActivityLogger.git", from: "3.4.0"),
-        .package(url: "https://github.com/Moya/Moya.git", from: "15.0.3"),
-        .package(url: "https://github.com/krzyzanowskim/CryptoSwift.git", from: "1.7.2"),
-        .package(url: "https://github.com/kishikawakatsumi/KeychainAccess.git", from: "4.2.2"),
-        .package(url: "https://github.com/kasketis/netfox.git", from: "1.21.0"),
-        .package(url: "https://github.com/vector-im/swift-ogg.git", from: "0.0.1"),
-        .package(url: "https://github.com/Quick/Nimble.git", from: "12.1.0"),
-        .package(url: "https://github.com/mac-cain13/R.swift.Library", from: "5.4.0")
+        .package(url: "https://github.com/ardito-daisuke-ebina/swift-ogg.git", from: "0.0.5"),
+        .package(url: "https://github.com/element-hq/opus-swift.git", from: "0.8.4"),
+        .package(url: "https://github.com/element-hq/ogg-swift.git", from: "0.8.3"),
         
         
     ],
@@ -36,19 +26,9 @@ let package = Package(
             name: "EbinaFrameworkWrapper",         // <--- new wrapper
             dependencies: [
 
-                .product(name: "Alamofire", package: "Alamofire"),
-                .product(name: "netfox", package: "netfox"),
-                .product(name: "WebRTC", package: "WebRTC"),
-                .product(name: "SwiftOGG", package: "swift-ogg"),
-                .product(name: "CryptoSwift", package: "CryptoSwift"),
-                .product(name: "Nimble", package: "Nimble"),
-                .product(name: "Starscream", package: "Starscream"),
-                .product(name: "AudioKit", package: "AudioKit"),
-                .product(name: "RxSwift", package: "RxSwift"),
-                .product(name: "AlamofireNetworkActivityLogger", package: "AlamofireNetworkActivityLogger"),
-                .product(name: "Moya", package: "Moya"),
-                .product(name: "Rswift", package: "R.swift.Library"),
-                .product(name: "KeychainAccess", package: "KeychainAccess"),
+                .product(name: "Copustools", package: "swift-ogg"),
+                .product(name: "YbridOpus", package: "opus-swift"),
+                .product(name: "YbridOgg", package: "ogg-swift"),
                 .target(name: "EbinaFramework")    // <-- reference the actual binary target here
             ],
             path: "Sources/ebinawrapper",
